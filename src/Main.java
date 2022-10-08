59f05f5566e99dc43c31030a5b33a49041fc525e
Sajer24 <sajer.kanat1992@gmail.com>
2022-10-08 19:10:36 +0300

    мини-задачка по курсовому

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(square(12));
+        System.out.println(square2(12.2));
+    }
+    public static double square(double x){
+        double y = x*x;
+        return y;
+    }
+
+    public static double square2(double x){
+        double y = Math.pow(x, 2);
+        return y;
     }
 }
